6754466d147b327a254fc42f98194d94c4bf4c34
subanandhini <subanandhini08gmail.com>
2022-11-17 22:17:03 +0530

    changes in one and two.py files

--- module/one.py
+++ module/one.py
@@ -1,20 +1,36 @@
 class this_is_one:
     a=100
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, name, age, gender):
+        self.fullname = name
+        self.personage = age
+        self.gender=gender
 
     def first(self):
-        return (self.name, self.age)
+        return (self.fullname)
+    def second(self): 
+        return (self.personage, self.gender)
+    def third(self):
+        return (int(self.personage))
+    def fourth(self):
+        return (self.gender)
 
-
-a = {1, 2, 11}
+a = {1, 2, 11, 13,14 }
 person1 = {
     "name": "John",
     "age": 36,
     "country": "Norway"
 }
-# i=this_is_one("subanandhini", 24)
+nandhini=this_is_one("subanandhini", 24, "female")
+print(nandhini.first())
+print(nandhini.second())
+print(nandhini.third())
+print(nandhini.fourth())
+nandhini2=this_is_one("Nandhini2", 21, "female")
+print(nandhini2.first())
+print(nandhini2.second())
+print(nandhini2.third())
+print(nandhini2.fourth())
+# i=this_is_one("subanandhini", 24, "female")
 # print(i.first())
 # j=this_is_one("dhivyakaran", 20)
 # print(j.first())
