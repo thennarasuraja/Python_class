6452f951614cc2fb8d4fe6b472d0299e4753f1c7
Nandhini k <subanandhini08@gmail.com>
2022-11-17 19:12:37 +0530

    Add files via upload

---
+++ module/one.py
@@ -0,0 +1,20 @@
+class this_is_one:
+    a=100
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def first(self):
+        return (self.name, self.age)
+
+
+a = {1, 2, 11}
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
+# i=this_is_one("subanandhini", 24)
+# print(i.first())
+# j=this_is_one("dhivyakaran", 20)
+# print(j.first())
---
+++ module/two.py
@@ -0,0 +1,15 @@
+from  one import this_is_one as i
+class this_is_two:
+    def __init__(self, fname, lname):
+        self.fname =fname
+        self.lname = lname
+    def second(self):
+        return (self.fname, self.lname)
+
+x=this_is_two("suba","nandhini")
+print(x.second())
+y=this_is_two("dhivya","karan")
+print(y.second())
+z=i("nandhini", 24)
+print(z.first())
+print(z.a)
