99f175d8967af499271a91053f2a632af09b7111
subanandhini <subanandhini08gmail.com>
2022-11-21 22:05:47 +0530

    added math

---
+++ module/math/math.py
@@ -0,0 +1,43 @@
+import math
+x = min(5.8, 10, 25)
+y = max(5, 10, 25)
+
+# print(x)
+# print(y)
+# z=abs(-127.25)
+# print(z)
+# power=pow(x, y)
+# print(power)
+# squareroot= math.sqrt(64)
+# print(squareroot)
+# ceiling=math.ceil(1.0000000000000001)
+# floor =math.floor(1.9999999999999998)
+# print(ceiling)
+# print(floor)
+# pi_value= math.pi
+# print(pi_value)
+# k = 3
+# n =5
+# print(math.comb(2, 10))
+# 1 , 2 ,3 ,4, 5
+# 123
+# 234
+# 345
+# 451
+# 512
+# 513
+# 425
+# print(math.fabs(x))
+# print(math.factorial(int(abs(x))))
+# print((sum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1])))
+
+# print((math.fsum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1])))
+# print(math.gcd(2, 4))
+print (math.isnan (56))
+print (math.isnan (-45.34))
+print (math.isnan (+45.34))
+print (math.isnan (math.inf))
+print (math.isnan (float("nan")))
+print (math.isnan (float("inf")))
+print (math.isnan (float("-inf")))
+print (math.isnan (math.nan))
