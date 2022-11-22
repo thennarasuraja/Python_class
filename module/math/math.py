3e4ce72040e32f8b64e905fa053673f88aa0800a
subanandhini <subanandhini08gmail.com>
2022-11-22 21:50:14 +0530

    math part2

--- module/math/math.py
+++ module/math/math.py
@@ -33,12 +33,28 @@ y = max(5, 10, 25)
 
 # print((math.fsum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1])))
 # print(math.gcd(2, 4))
-print (math.isnan (56))
-print (math.isnan (-45.34))
-print (math.isnan (+45.34))
-print (math.isnan (math.inf))
-print (math.isnan (float("nan")))
-print (math.isnan (float("inf")))
-print (math.isnan (float("-inf")))
-print (math.isnan (math.nan))
-print("added to main")
+# print (math.isnan (56))
+# print (math.isnan (-45.34))
+# print (math.isnan (+45.34))
+# print (math.isnan (math.inf))
+# print (math.isnan (float("nan")))
+# print (math.isnan (float("inf")))
+# print (math.isnan (float("-inf")))
+# print (math.isnan (math.nan))
+# print("added to main")
+# print()
+# print(math.isfinite(x))
+# print(math.isfinite(float("Nan")))
+# print(math.isnan(float("Nan")))
+# print(math.isnan(float(90)))
+# print(math.isnan(int(90)))
+# print(math.lcm(10,2,5,6))   #30
+# print(math.lcm(10,2,5,16,18))  #720
+# print(math.gcd(10,2,16,18))  #2
+print(math.remainder(12.6, 5))   #12.6/5 =  15-2.4=12.6,    10+2.6=12.6
+print(math.remainder(12.5, 5))   #12.5/5 =  15-2.5=12.5,    10+2.5=12.5S
+print(math.remainder(12.4, 5))   #12.4/5 =  15-2.6=12.4,    10+2.4=12.4
+# 10 15 20
+# 10  12.5  15
+# print(math.nextafter(2.09,30.0))
+
